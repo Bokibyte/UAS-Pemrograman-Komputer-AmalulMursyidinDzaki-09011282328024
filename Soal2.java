10aa1d793a698952fb4478b5d548de8c88afd2c0
Bokibyte <triplecreeperx@gmail.com>
2023-12-05 23:13:30 +0700

    selesai

--- Soal2.java
+++ Soal2.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Soal2 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         String username = "jekiSadboyAmbyar";
-        String password = "kakSepti";
-
+        String password = "kakSepti6969awwwwww";
+        System.out.print("Masukan Username:");
+        String inUser = sc.nextLine();
+        System.out.print("Masukan Password:");
+        String inPassword = sc.nextLine();
+        if(inUser.equals(username)){
+            if(inPassword.equals(password)){
+                
+            System.out.print("Autentikasi Berhasil!");
+            }else{
+                
+            System.out.print("Autentikasi Gagal!");
+            }
+        }else{
+            System.out.print("Autentikasi Gagal!");
+        }
     }
 }
