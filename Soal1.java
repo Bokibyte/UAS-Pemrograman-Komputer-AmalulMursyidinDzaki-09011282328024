aed8aca651c976b44f7c80479c8adf319013f50f
Bokibyte <triplecreeperx@gmail.com>
2023-12-05 15:47:33 +0700

    puin

--- Soal1.java
+++ Soal1.java
@@ -6,5 +6,11 @@ public static void main(String[] args) {
     String tescomit = "sedih";
     int ufsifhesefnw = "86249826498329";
     int p = 0;
+    int x = 0;
+    int tes = 0;
+    int p = 0;
+    int qq = 0;
+    String ssss = 12;
+    
     }
 }
