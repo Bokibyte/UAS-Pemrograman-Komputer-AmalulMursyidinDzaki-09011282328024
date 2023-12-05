cd3233c3ea69ff480a36a25dcedd9e14791007b3
Bokibyte <triplecreeperx@gmail.com>
2023-12-05 23:07:51 +0700

    membuat scanner ddan variabel

--- Soal2.java
+++ Soal2.java
@@ -1,5 +1,8 @@
 public class Soal2 {
     public static void main(String[]args){
-        
+        Scanner sc = new Scanner(System.in);
+        String username = "jekiSadboyAmbyar";
+        String password = "kakSepti";
+
     }
 }
