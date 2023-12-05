2cc289e8da17beb0fd656c015a36343916a99140
Bokibyte <triplecreeperx@gmail.com>
2023-12-05 15:38:34 +0700

    lanjt

--- Soal1.java
+++ Soal1.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Soal1{
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    Strul
+    String punten = "";
+    String tescomit = "sedih";
+    int ufsifhesefnw = "86249826498329";
     }
 }
