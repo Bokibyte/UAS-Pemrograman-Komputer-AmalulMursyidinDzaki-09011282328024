ebb4322f10436251f01d1ee56a96c27277ebfad9
Bokibyte <triplecreeperx@gmail.com>
2023-12-05 18:58:47 +0700

    membuat data produk & harga dan data arraylist

--- Soal1.java
+++ Soal1.java
@@ -1,16 +1,5 @@
-import java.util.*;
-public class Soal1{
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String punten = "";
-    String tescomit = "sedih";
-    int ufsifhesefnw = "86249826498329";
-    int p = 0;
-    int x = 0;
-    int tes = 0;
-    int p = 0;
-    int qq = 0;
-    String ssss = 12;
-    
+class Soal1{
+    public static void main(String[]args){
+        
     }
 }
